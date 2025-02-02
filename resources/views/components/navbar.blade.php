913225a07eef78a91dbfc5d58249006428be610f
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-02-02 14:39:52 +0700

    update button logout

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -48,10 +48,7 @@
                         <ul @click.away="closeNotificationsMenu" @keydown.escape="closeNotificationsMenu" class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:text-gray-300 dark:border-gray-700 dark:bg-gray-800">
                         <li class="flex">
                             <a class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200" href="#">
-                            <span>Notification</span>                            
-                            <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-600 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-600">
-                                13
-                            </span>
+                                <span>Notification</span>
                             </a>
                         </li>
                         <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -155,17 +155,19 @@
                 </li>
             </ul>
             <!-- Tombol Logout -->
-            <div class="mt-auto px-6 py-3">
-                <form method="POST" action="/auth/logout">
-                    @csrf
-                    <button type="submit" class="flex items-center w-full px-4 py-2 text-sm font-semibold leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-red-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-red">
-                        <div class="invert-icon">
-                            <img src="{{ asset('assets/img/menu-icons/logout.png') }}" alt="Logout Icon" class="h-5 w-5">
-                        </div>
-                        <span class="ml-4">logout</span>
-                    </button>
-                </form>
-            </div>
+             <ul>
+                <li class="relative px-6 py-3">
+                    <form method="POST" action="/auth/logout">
+                        @csrf
+                        <button type="submit" class="flex items-center w-full text-sm font-semibold text-gray-800 dark:text-gray-100">
+                            <div class="invert-icon">
+                                <img src="{{ asset('assets/img/menu-icons/logout.png') }}" alt="Logout Icon by Pixel perfect" class="h-5 w-5 mr-2">
+                                <span>Logout</span>
+                            </div>
+                        </button>
+                    </form>
+                </li>
+             </ul>
         </div>
     </aside>
 
@@ -325,17 +327,19 @@
                 </li>
             </ul>
             <!-- Tombol Logout -->
-            <div class="mt-auto px-6 py-3">
-                <form method="POST" action="/auth/logout">
-                    @csrf
-                    <button type="submit" class="flex items-center w-full px-4 py-2 text-sm font-semibold leading-5 text-white transition-colors duration-150 bg-red-600 border border-transparent rounded-lg active:bg-red-600 hover:bg-red-700 focus:outline-none focus:shadow-outline-red">
-                        <div class="invert-icon">
-                            <img src="{{ asset('assets/img/menu-icons/logout.png') }}" alt="Logout Icon" class="h-5 w-5">
-                        </div>
-                        <span class="ml-4">logout</span>
-                    </button>
-                </form>
-            </div>
+            <ul>
+                <li class="relative px-6 py-3">
+                    <form method="POST" action="/auth/logout">
+                        @csrf
+                        <button type="submit" class="flex items-center w-full text-sm font-semibold text-gray-800 dark:text-gray-100">
+                            <div class="invert-icon">
+                                <img src="{{ asset('assets/img/menu-icons/logout.png') }}" alt="Logout Icon by Pixel perfect" class="h-5 w-5 mr-2">
+                                <span>Logout</span>
+                            </div>
+                        </button>
+                    </form>
+                </li>
+             </ul>
         </div>
     </aside>
 </div>
