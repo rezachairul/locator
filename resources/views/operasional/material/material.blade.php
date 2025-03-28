0ea34dc914997cf0a4f45f444b8d9ea3ac513b24
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-03-28 14:53:01 +0700

    Update fitur edit materials

--- resources/views/operasional/material/material.blade.php
+++ resources/views/operasional/material/material.blade.php
@@ -66,7 +66,7 @@
                                     <div id="updateProductModal-{{ $material->id }}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                         <div class="relative w-full max-w-2xl max-h-full">
                                             <!-- Modal content -->
-                                            <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-700">
+                                            <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
                                                 <!-- Modal header -->
                                                 <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                                                     <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
