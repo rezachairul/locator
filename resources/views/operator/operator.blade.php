5d8edab55697f4c578ab3d82cf8f5de3175b145e
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-04-12 10:54:55 +0700

    Refactor action buttons in user table for improved styling and accessibility

--- resources/views/operator/operator.blade.php
+++ resources/views/operator/operator.blade.php
@@ -89,14 +89,15 @@
                             <td class="px-2 py-1 text-center">{{ $user->email }}</td>
                             <td class="px-2 py-1 text-center">********</td>
                             <td class="px-2 py-1 text-center">{{ $user->role }}</td>
-                            <td class="px-2 py-1 text-center">
-                                <div class="flex item-center justify-center space-x-2 text-sm">
-                                    <!-- Button Edit -->
+                            <!-- Actions -->
+                            <td class="px-2 py-1 text-xs text-center">
+                                <div class="flex justify-center space-x-2">
+                                    <!-- Edit -->
                                     <button id="updateProductButton-{{ $user->id }}" data-modal-target="updateProductModal-{{ $user->id }}" data-modal-toggle="updateProductModal-{{ $user->id }}" type="button"
-                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                        class="inline-block text-gray-500 hover:text-yellow-600 transition-colors duration-200"
                                         aria-label="Edit">
-                                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                         </svg>
                                     </button>
                                     <!-- Modal Edit -->
@@ -126,7 +127,7 @@
                                                     @csrf
                                                     @method('PUT')
                                                     <!-- Form Fields -->
-
+        
                                                     <!-- Name Field -->
                                                     <div>
                                                         <label for="name" class="block text-sm font-medium text-gray-900 dark:text-white text-left mt-2">Name</label>
@@ -194,7 +195,7 @@
                                                             const nameUpdate = document.getElementById('name-update-{{ $user->id }}');
                                                             const roleUpdate = document.getElementById('role-update-{{ $user->id }}');
                                                             const emailUpdate = document.getElementById('email-update-{{ $user->id }}');
-
+        
                                                             function generateEmailUpdate() {
                                                                 const nameValue = nameUpdate.value.trim().split(" ")[0].toLowerCase();
                                                                 const roleValue = roleUpdate.value;
@@ -204,18 +205,18 @@
                                                                     emailUpdate.value = '';
                                                                 }
                                                             }
-
+        
                                                             // Event ketika modal dibuka → trigger update email
                                                             const observer = new MutationObserver(() => {
                                                                 if (!nameUpdate || !roleUpdate) return;
                                                                 generateEmailUpdate();
                                                             });
-
+        
                                                             const modal = document.getElementById('updateProductModal-{{ $user->id }}');
                                                             if (modal) {
                                                                 observer.observe(modal, { attributes: true, attributeFilter: ['class'] });
                                                             }
-
+        
                                                             nameUpdate.addEventListener('input', generateEmailUpdate);
                                                             roleUpdate.addEventListener('change', generateEmailUpdate);
                                                         });
@@ -224,10 +225,12 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <!-- Button Delete -->
-                                    <button id="deleteButton-{{ $user->id }}" data-modal-target="deleteModal-{{ $user->id }}" data-modal-toggle="deleteModal-{{ $user->id }}" type="button" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete">
-                                        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+                                    <!-- Delete -->
+                                    <button id="deleteButton-{{ $user->id }}" data-modal-target="deleteModal-{{ $user->id }}" data-modal-toggle="deleteModal-{{ $user->id }}" type="button"
+                                    class="inline-block text-gray-500 hover:text-red-600 transition-colors duration-200"
+                                    aria-label="Delete">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                         </svg>
                                     </button>
                                     <!-- Modal Delete -->
