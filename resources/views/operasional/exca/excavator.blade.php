df651c25c0f5b3e7139eba3adc433345856f0d8c
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-04-12 13:49:24 +0700

    Refactor buttons in excavator forms for improved styling and accessibility

--- resources/views/operasional/exca/excavator.blade.php
+++ resources/views/operasional/exca/excavator.blade.php
@@ -135,9 +135,6 @@
                                     <div class="flex items-center text-xs">
                                         <div class="items-center">
                                             <p class="font-semibold">{{ $exca->loading_unit }}</p>
-                                            <!-- <p class="text-xs text-gray-600 dark:text-gray-400">
-                                                Belum ada relasi
-                                            </p> -->
                                         </div>
                                     </div>
                                 </td>
@@ -162,15 +159,19 @@
                                 <td class="px-2 py-1 text-xs text-center">
                                     {{ $exca->created_at->format('d-m-Y')}}
                                 </td>
-                                <td class="px-2 py-1 text-center">
-                                    <div class="flex item-center justify-center space-x-4 text-sm">
-                                        <!-- Update -->
-                                        <button id="updateProductButton-{{ $exca->id }}" data-modal-target="updateProductModal-{{ $exca->id }}" data-modal-toggle="updateProductModal-{{ $exca->id }}" type="button" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Edit" >
-                                            <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" >
-                                                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" ></path>
+
+                                <!-- Actions -->
+                                <td class="px-2 py-1 text-xs text-center">
+                                    <div class="flex justify-center space-x-4">
+                                        <!-- Edit -->
+                                        <button id="updateProductButton-{{ $exca->id }}" data-modal-target="updateProductModal-{{ $exca->id }}" data-modal-toggle="updateProductModal-{{ $exca->id }}" type="button"
+                                            class="inline-block text-gray-500 hover:text-yellow-600 transition-colors duration-200"
+                                            aria-label="Edit">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                             </svg>
                                         </button>
-                                        <!-- Modal Update -->
+                                        <!-- Modal Edit -->
                                         <div id="updateProductModal-{{ $exca->id }}" tabindex="-1" aria-hidden="true"
                                             class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                             <div class="relative w-full max-w-2xl max-h-full">
@@ -245,25 +246,29 @@
                                                                     </div>
                                                                 </div>
                                                             </div>
-                                                            <button type="submit"
-                                                                class="text-white inline-flex m-1 items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                                                                <svg class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                                                    xmlns="http://www.w3.org/2000/svg">
-                                                                    <path fill-rule="evenodd"
-                                                                        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                                                        clip-rule="evenodd"></path>
-                                                                </svg>
-                                                                Update Data {{$title}}
-                                                            </button>
-                                                            <button type="button" data-modal-toggle="updateProductModal-{{ $exca->id }}"
-                                                                class="text-red-600 inline-flex m-1 items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
-                                                                <svg class="mr-1 -ml-1 w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
-                                                                    height="24" fill="none" viewBox="0 0 24 24">
-                                                                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
-                                                                        d="m6 6 12 12m3-6a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                                                </svg>
-                                                                Cancel
-                                                            </button>
+
+                                                            <!-- Submit and Cancel Buttons -->
+                                                             <div class="flex justify-end space-x-2 pt-4">
+                                                                 <button type="submit"
+                                                                     class="text-white inline-flex m-1 items-center bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
+                                                                     <svg class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                                                         xmlns="http://www.w3.org/2000/svg">
+                                                                         <path fill-rule="evenodd"
+                                                                             d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                                                             clip-rule="evenodd"></path>
+                                                                     </svg>
+                                                                     Update Data {{$title}}
+                                                                 </button>
+                                                                 <button type="button" data-modal-toggle="updateProductModal-{{ $exca->id }}"
+                                                                     class="text-red-600 inline-flex m-1 items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+                                                                     <svg class="mr-1 -ml-1 w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                                                                         height="24" fill="none" viewBox="0 0 24 24">
+                                                                         <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                                                             d="m6 6 12 12m3-6a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                                                     </svg>
+                                                                     Cancel
+                                                                 </button>
+                                                             </div>
                                                         </form>
                                                     @else
                                                     <p>Data tidak ditemukan.</p>                                                 
@@ -271,13 +276,18 @@
                                                 </div>
                                             </div>
                                         </div>
+                                         
+
                                         <!-- Delete -->
-                                        <button id="deleteButton-{{ $exca->id }}" data-modal-target="deleteModal-{{ $exca->id }}" data-modal-toggle="deleteModal-{{ $exca->id }}" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray" aria-label="Delete" type="button">
-                                            <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" >
-                                                <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" ></path>
-                                            </svg>                                            
+                                        <button id="deleteButton-{{ $exca->id }}" data-modal-target="deleteModal-{{ $exca->id }}" data-modal-toggle="deleteModal-{{ $exca->id }}" type="button"
+                                        class="inline-block text-gray-500 hover:text-red-600 transition-colors duration-200"
+                                        aria-label="Delete">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
                                         </button>
-                                        <!-- delete button modals -->
+                                        <!-- Modal Delete -->
+                                         
                                         <div id="deleteModal-{{ $exca->id }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
                                             <div class="relative p-4 w-full max-w-md h-full md:h-auto">
                                                 <!-- Modal content -->
@@ -306,7 +316,7 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </div>                                        
                                     </div>
                                 </td>
                             </tr>
