1e41117e6fa29907c34248a1dbdb1a8e43aee548
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-04-27 08:42:49 +0700

    Enhance logout button functionality: change button to type="submit" for proper form submission in login and user report views

--- resources/views/auth/login/login.blade.php
+++ resources/views/auth/login/login.blade.php
@@ -76,10 +76,10 @@
                     <form action="/auth/login" method="post">
                         @csrf
                         <div class="block text-sm mt-8">
-                            <input type="email" name="email" id="email" class=" block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="name@example.com" required autofocus value="{{old('email')}}">
+                            <input type="email" name="email" id="email" class=" block w-full mt-1 text-gray-900 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="name@example.com" required autofocus value="{{old('email')}}">
                         </div>
                         <div class="block text-sm mt-8">
-                            <input type="password" name="password" id="password" class=" block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Password" required>
+                            <input type="password" name="password" id="password" class=" block w-full mt-1 text-gray-900 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Password" required>
                         </div>
                         <button type="submit" class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">Log In</button>
                     </form>
