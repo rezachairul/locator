b657b6a04a625b2f0b853467e3aaf876a768fba2
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-07-06 21:43:04 +0700

    Update comment for edit and delete buttons in incident user report table for future use

--- resources/views/admin/laporan/partials/table_body.blade.php
+++ resources/views/admin/laporan/partials/table_body.blade.php
@@ -76,7 +76,7 @@
                         </a>
                     </button>
 
-                    <!-- Use Future -->
+                    <!-- Tombol Edit & Delete (future use) -->
                     <!-- Edit -->
                     <!-- <button class="inline-block text-gray-500 hover:text-yellow-600 transition-colors duration-200">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
