70c337cb33a823a8f38eb6959bafa6a431644ad3
Reza Chairul <98737769+rezachairul@users.noreply.github.com>
2025-07-18 10:00:29 +0700

    `Added file format and size restrictions to create map form`

--- resources/views/admin/maps/create.blade.php
+++ resources/views/admin/maps/create.blade.php
@@ -26,6 +26,7 @@
                     <div>
                         <label for="file" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama File</label>
                         <input type="file" name="file" id="file" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="file" required="">
+                        <p class="mt-1 text-sm text-gray-500 dark:text-gray-300">Format file yang didukung: mbtiles, ecw, tif, tiff. Ukuran maksimal: 100MB.</p>
                     </div>
                 </div>
                 <div class="flex justify-end space-x-2 pt-4">
